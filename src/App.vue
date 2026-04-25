f59f0748d7d6284907c99d2e689f1e6ab4408bc1
DavinVatanak <e20220234@dtc1.itc.edu.kh>
2026-04-25 07:27:09 +0700

    Static Landing page include navigation and footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import Nav from './components/common/Navbar.vue'
+import Home from './pages/landingPage.vue'
+import Footer from './components/common/Footer.vue'
 </script>
 
 <template>
- Hello World
+  <div class="min-h-screen bg-white">
+    <Nav />
+    <Home />
+    <Footer />
+  </div>
+
 </template>
