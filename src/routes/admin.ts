85283bdc84ce03b9b219838929410e5a881ff99b
sanaSOK <sanasokitc2023@gmail.com>
2026-05-01 02:15:59 +0700

    update route admin

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,10 +7,10 @@ export default [
         children: [
             { path: 'dashboard', name: 'admin-dashboard', component: () => import('../pages/admin/page.vue') },
             { path: 'books', name: 'admin-books', component: () => import('../pages/admin/Books.vue') },
-            { path: 'categories', name: 'admin-categories', component: () => import('../pages/admin/Categories.vue') },
+            
             { path: 'users', name: 'admin-users', component: () => import('../pages/admin/Users.vue') },
             { path: 'notifications', name: 'admin-notifications', component: () => import('../pages/admin/Notifications.vue') },
-            { path: 'roles', name: 'admin-roles', component: () => import('../pages/admin/Roles.vue') },
+            
             { path: 'comments', name: 'admin-comments', component: () => import('../pages/admin/Comments.vue') },
             { path: 'reports', name: 'admin-reports', component: () => import('../pages/admin/Reports.vue') },
             { path: 'settings', name: 'admin-settings', component: () => import('../pages/admin/Settings.vue') },
